d40ececc5b3ee1877442eab1cb6e1c3bdf51f9c0
Mister Milian <biloramez@gmail.com>
2026-02-12 00:35:59 +0200

    Create NetworkActions.kt

---
+++ app/src/main/kotlin/com/example/tasalicool/network/NetworkActions.kt
@@ -0,0 +1,9 @@
+package com.example.tasalicool.network
+
+object NetworkActions {
+    const val PLAYER_JOINED = "PLAYER_JOINED"
+    const val PLAYER_LEFT = "PLAYER_LEFT"
+    const val GAME_START = "GAME_START"
+    const val PLAY_CARD = "PLAY_CARD"
+    const val UPDATE_SCORE = "UPDATE_SCORE"
+}
